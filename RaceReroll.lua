fb8e1f2e06ce009467134ab72969763f115a7d3a
vCWEYmED6Y <98236559+vCWEYmED6Y@users.noreply.github.com>
2024-04-02 21:38:28 +0200

    ?? fixed shardwait again?

--- RaceReroll.lua
+++ RaceReroll.lua
@@ -210,7 +210,7 @@ sGui:SetCore("SendNotification", {
     Text = ("Rollback was initiated. Using lineage shard...");
     Duration = 1
 })
-task.wait(_G.Settings.ShardWait)
+task.wait(_settings.ShardWait)
 inventoryRemote:FireServer("Use", "Lineage Shard", lineageShard)
 task.wait(1)
 local dialogueRemote = p.PlayerGui:WaitForChild("NPCDialogue"):WaitForChild("RemoteEvent")
